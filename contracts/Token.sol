e05b5912b4ca70b25daafa39a8a54eb600b3aed0
Nuriee Cowan <nuriee.d@Nuriees-MBP.lan>
2023-04-30 19:42:37 -0500

    6. Token approvals

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -11,6 +11,7 @@ contract Token {
 
 	
 	mapping(address => uint256) public balanceOf;
+	mapping(address => mapping(address => uint256)) public allowance; 
 	
 	event Transfer(
 		address indexed from, 
@@ -18,6 +19,11 @@ contract Token {
 		uint256 value
 	);
 
+	event Approval(
+		address indexed owner, 
+		address indexed spender,
+		uint256 value
+	);
 
 	constructor(
 		string memory _name, 
@@ -42,6 +48,19 @@ contract Token {
 
 		emit Transfer(msg.sender, _to, _value);
 
+		return true;
+	}
+
+
+	function approve(address _spender, uint256 _value) 
+	public 
+	returns(bool success) 
+	{
+		require(_spender != address(0));
+
+		allowance[msg.sender][_spender] = _value; 
+
+		emit Approval(msg.sender, _spender, _value);
 		return true;
 	}
 }
